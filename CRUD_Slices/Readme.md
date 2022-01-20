e882db784ef22dbb940b105aeeca495d4b91cc2a
Pradhumn Sharma <pradhumn.s_by@external.swiggy.in>
2022-01-20 10:13:40 +0530

    Added readme files and fixed a mistake

---
+++ CRUD_Slices/Readme.md
@@ -0,0 +1,38 @@
+## Introduction
+
+In this folder, I have implemented the CRUD operation on Students and Teachers. I have learned about Struct, Nested Structs, Anonymous Structs, Slices, Http routing and controllers.
+
+I have provided dummy json for students and teachers. I have created separate struct for Response type.
+
+
+### How to use
+
+* Start the application using ### `go run main.go`. Application will start on `http://localhost:6001`.
+* Install Thunder client extension if using VS code or Postman for making api calls
+* Select data from StudentsDummy.json for Students and TeachersDummy.json for Teachers
+
+**Note:** For Students RollNo is unique identifier and for Teachers Id is unique identifier. When performing create operation these will generate randomly for first entry and then increment by 1 for rest entries.
+
+Following operations are available for both tables
+
+1. Create
+2. Update
+3. Get All
+4. Get One
+5. Delete All
+6. Delete One
+
+
+### What I Learned
+1. How to create random numbers
+2. How to use Structs and Nested structs
+3. How to modify response keys as per UI requirements
+4. How to hide a field from not showing on UI
+5. How to use params for updating and deleting
+6. How to use query params for  updating and deleting (Not implemented in this but other place)
+7. How to convert string to number and number to string
+8. Better understanding of pointer, reference and copy variable.
+
+
+
+### `Note:` Please review the work. All suggestions related to improvements are highly welcome and appreciated.
--- CRUD_Slices/main.go
+++ CRUD_Slices/main.go
@@ -47,8 +47,10 @@ var teachersList []Teacher
 func main() {
 	fmt.Println("CRUD")
 	r := mux.NewRouter()
+
 	r.HandleFunc("/", serveHome)
 
+	// Students Routes
 	r.HandleFunc("/create-student", createStudent).Methods("POST")
 	r.HandleFunc("/students", getAllStudents).Methods("GET")
 	r.HandleFunc("/students/{roll_no}", getStudentDetails).Methods("GET")
@@ -56,6 +58,7 @@ func main() {
 	r.HandleFunc("/students/{roll_no}", deleteStudent).Methods("DELETE")
 	r.HandleFunc("/students", deleteAllStudent).Methods("DELETE")
 
+	// Teachers Routes
 	r.HandleFunc("/create-teacher", createTeacher).Methods("POST")
 	r.HandleFunc("/teachers", getAllTeachers).Methods("GET")
 	r.HandleFunc("/teachers/{id}", getTeacherDetails).Methods("GET")
@@ -63,9 +66,11 @@ func main() {
 	r.HandleFunc("/teachers/{id}", deleteTeacher).Methods("DELETE")
 	r.HandleFunc("/teachers", deleteAllTeacher).Methods("DELETE")
 
+	// Start server
 	log.Fatal(http.ListenAndServe(":6001", r))
 }
 
+// Static HTML Page
 func serveHome(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("<div><h1>Welcome to Fake Crud</h1><p>Here we will create crud with simple slices and struct without any database.<br>We will create students and teachers and perform operations on them</p></div>"))
 }
@@ -96,41 +101,9 @@ func createStudent(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(response)
 }
 
-func createTeacher(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	if r.Body == nil {
-		json.NewEncoder(w).Encode("Please add all details")
-		return
-	}
-	var teacher Teacher
-	json.NewDecoder(r.Body).Decode(&teacher)
-	if len(teachersList) > 0 {
-		for _, teach := range studentsList {
-			if teach.Email == teacher.Email {
-				response := Response{0, "Teacher already exist", teach}
-				json.NewEncoder(w).Encode(response)
-				return
-			}
-		}
-		teacher.Id = teachersList[len(teachersList)-1].Id + 1
-	} else {
-		rand.Seed(time.Now().UnixNano())
-		teacher.Id = rand.Intn(4000)
-	}
-	teachersList = append(teachersList, teacher)
-	response := Response{1, "Teacher added successfully", teacher}
-	json.NewEncoder(w).Encode(response)
-}
-
 func getAllStudents(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	response := Response{1, "Students fetched successfully", studentsList}
-	json.NewEncoder(w).Encode(response)
-}
-
-func getAllTeachers(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	response := Response{1, "Teachers fetched successfully", teachersList}
 	json.NewEncoder(w).Encode(response)
 }
 
@@ -144,19 +117,6 @@ func getStudentDetails(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 	response := Response{1, "Student details not found", nil}
-	json.NewEncoder(w).Encode(response)
-}
-
-func getTeacherDetails(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	for _, teacher := range teachersList {
-		if strconv.Itoa(teacher.Id) == params["id"] {
-			response := Response{1, "Teacher details found", teacher}
-			json.NewEncoder(w).Encode(response)
-			return
-		}
-	}
-	response := Response{1, "Teacher details not found", nil}
 	json.NewEncoder(w).Encode(response)
 }
 
@@ -183,6 +143,73 @@ func updateStudentDetails(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(response)
 }
 
+func deleteStudent(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, student := range studentsList {
+		if strconv.Itoa(student.RollNo) == params["roll_no"] {
+			studentsList = append(studentsList[:index], studentsList[index+1:]...)
+			response := Response{1, "Student details deleted successfully", student}
+			json.NewEncoder(w).Encode(response)
+			return
+		}
+	}
+	response := Response{1, "No student to delete", nil}
+	json.NewEncoder(w).Encode(response)
+}
+
+func deleteAllStudent(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	studentsList = []Student{}
+	response := Response{1, "All students deleted successfully", nil}
+	json.NewEncoder(w).Encode(response)
+}
+
+func createTeacher(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	if r.Body == nil {
+		json.NewEncoder(w).Encode("Please add all details")
+		return
+	}
+	var teacher Teacher
+	json.NewDecoder(r.Body).Decode(&teacher)
+	if len(teachersList) > 0 {
+		for _, teach := range teachersList {
+			if teach.Email == teacher.Email {
+				response := Response{0, "Teacher already exist", teach}
+				json.NewEncoder(w).Encode(response)
+				return
+			}
+		}
+		teacher.Id = teachersList[len(teachersList)-1].Id + 1
+	} else {
+		rand.Seed(time.Now().UnixNano())
+		teacher.Id = rand.Intn(4000)
+	}
+	teachersList = append(teachersList, teacher)
+	response := Response{1, "Teacher added successfully", teacher}
+	json.NewEncoder(w).Encode(response)
+}
+
+func getAllTeachers(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	response := Response{1, "Teachers fetched successfully", teachersList}
+	json.NewEncoder(w).Encode(response)
+}
+
+func getTeacherDetails(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	for _, teacher := range teachersList {
+		if strconv.Itoa(teacher.Id) == params["id"] {
+			response := Response{1, "Teacher details found", teacher}
+			json.NewEncoder(w).Encode(response)
+			return
+		}
+	}
+	response := Response{1, "Teacher details not found", nil}
+	json.NewEncoder(w).Encode(response)
+}
+
 func updateTeacherDetails(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	if r.Body == nil {
@@ -206,21 +233,6 @@ func updateTeacherDetails(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(response)
 }
 
-func deleteStudent(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r)
-	for index, student := range studentsList {
-		if strconv.Itoa(student.RollNo) == params["roll_no"] {
-			studentsList = append(studentsList[:index], studentsList[index+1:]...)
-			response := Response{1, "Student details deleted successfully", student}
-			json.NewEncoder(w).Encode(response)
-			return
-		}
-	}
-	response := Response{1, "No student to delete", nil}
-	json.NewEncoder(w).Encode(response)
-}
-
 func deleteTeacher(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
@@ -233,13 +245,6 @@ func deleteTeacher(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 	response := Response{1, "No teacher to deleted", nil}
-	json.NewEncoder(w).Encode(response)
-}
-
-func deleteAllStudent(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	studentsList = []Student{}
-	response := Response{1, "All students deleted successfully", nil}
 	json.NewEncoder(w).Encode(response)
 }
 
